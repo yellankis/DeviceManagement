f56bc33532d02df13240c16227fe1024faf387c6
yellankis <52095982+yellankis@users.noreply.github.com>
2019-07-13 17:15:16 +0530

    Update DeviceManagementController.java

--- DeviceManagementController.java
+++ DeviceManagementController.java
@@ -1,35 +1,42 @@
-package com.device.management.controller;
+package com.problem.shaheed.statement;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.device.management.service.DeviceManagementService;
-import com.device.management.model.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathParam;
+
+import com.problem.shaheed.dao.Device;
+import com.problem.shaheed.service.DeviceManagementService;
 
 
-@RestController("register")
+@RestController
 public class DeviceManagementController {
 
     @Autowired
     DeviceManagementService deviceManagementService;
 
-    @RequestMapping(value="/devices/list", method=RequestMethod.GET)
-    public ResponseBody getAllDevices{
-        List<Device> objects = new ArrayList<Device>();
-        return deviceManagementService.getAllDevices();
+  @RequestMapping(value="/devices/list", method=RequestMethod.GET)
+  public void createDevic(@RequestBody Device device){
+       List<Device> objects = new ArrayList<Device>();
+      deviceManagementService.createDevice(device);
 
-    }
+   }
 
     @RequestMapping(value="/device", method=RequestMethod.GET)
-    public ResponseBody getDevice(@PathParam Long id){
+    public Optional<com.problem.shaheed.dao.Device> getDevice(@RequestParam Long id){
         return deviceManagementService.getDevice(id);
 
     }
 
     @RequestMapping(value="/device/create", method=RequestMethod.GET)
-    public ResponseBody createDevice(@RequestBody Device device){
-        return deviceManagementService.createDevice(device);
+    public List<com.problem.shaheed.dao.Device> getAllDevices(){
+        return deviceManagementService.getAllDevices();
 
     }
 
